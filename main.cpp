4cbf1387674a63b5b83d0e50efbdcebb339e415c
Jacksonjames1998 <jacksonkalluvila@gmail.com>
2024-06-12 22:52:23 +0530

    Added table in DB

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<mysql.h>
 #include<mysqld_error.h>
+#include<sstream>
 using namespace std;
 
 const char* HOST = "localhost";
@@ -8,13 +9,52 @@ const char* USER = "root";
 const char* PW = "root";
 const char* DB = "mydb";
 
+class Hostel{
+private:
+    string name;
+    int bed;
+    int fee;
+public:
+    Hostel(string Name, int Bed, int Fee){
+        name = Name;
+        bed = bed;
+        fee = Fee;
+    }
+
+    string getName(){
+        return name;
+    }
+    int getBed(){
+        return bed;
+    }
+    int getFee(){
+        return fee;
+    }
+};
+
 int main(){
     MYSQL* conn;
     conn = mysql_init(NULL);
     if(!mysql_real_connect(conn, HOST, USER, PW,DB,3306,NULL,0)){
-        cout << "MYSQL connection failed";
+        cout << "ERROR :" << mysql_error(conn) << endl;
     }
     else{
         cout << "MYSQL connection success";
     }
+
+    Hostel hostelOne("Sharavana",5,5000);
+
+    int intBed = hostelOne.getBed();
+    stringstream ss;
+    ss<< intBed;
+
+    string stringBed = ss.str();
+
+    int intFee = hostelOne.getFee();
+    stringstream ssFee;
+    ssFee<< intBed;
+    string stringFee = ssFee.str();
+
+
+    string insert = "INSERT INTO hostel (Name, Bed, Fee) VALUES ('"+hostelOne.getName()+"','"+stringBed+"','"+intFee+"')"
 }
